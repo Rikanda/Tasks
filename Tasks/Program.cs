f89121e51f99966dfc2d463d0f4fd44f1cff2116
Natalya <soltk7@yandex.ru>
2021-02-06 21:40:45 +0300

    1

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+
 
 namespace Tasks
 {
@@ -6,7 +8,62 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Process[] procList = Process.GetProcesses();
+            for (int i = 0; i < procList.Length; i++)
+            {
+
+                Console.WriteLine($"Номер по порядку: {i} ; Имя процесса: {procList[i].ProcessName} ; Id процесса:  {procList[i].Id}");
+            }
+
+            Console.Write("Укажите имя для завершения процесса: ");
+            string name = Console.ReadLine();
+            Process[] anti = Process.GetProcessesByName(name);
+
+
+
+            foreach (Process killed in anti)
+
+            {
+                Console.WriteLine(killed);
+                try
+                {
+                    killed.Kill();
+                    
+                }
+
+                catch
+                {
+                    Console.WriteLine("Что-то пошло не так, возможно вам не хватает прав на это действие");
+                }
+                
+            }
+
+            Console.Write("Укажите Id для завершения процесса: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Process ant = Process.GetProcessById(n);
+            Console.WriteLine(ant);
+            try
+            {
+                ant.Kill();
+            }
+
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так, возможно вам не хватает прав на это действие");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
